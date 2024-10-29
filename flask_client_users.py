ff7d8b6b22781761cebdd417dc508cb0c13c8095
GaborCEH <bozsik.gabor@ceh.hu>
2024-10-29 20:26:06 +0100

    First file
    1. file

---
+++ flask_client_users.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 14 10:49:43 2023
+
+@author: pablo
+"""
+
+import requests
+
+
+base_url= 'http://127.0.0.1:5000'#https://database.pcalleja.linkeddata.es/
+url = base_url+'/users'
+response = requests.get(url)
+print(response.json())
+
+
+
+
+
+
+import requests
+url = 'https://database.pcalleja.linkeddata.es/users'
+data =  {
+    "classes": [
+      {
+        "name": "Programming"
+      }
+    ],
+    "name": "María",
+    "status": "professor",
+    "user": "mpoveda"
+  }
+response = requests.post(url,json=data)
+print(response.json())
